43932c8e5708226b62f3bc9e04be162bb0200301
IhuomaO <logzimalone@gmail.com>
2019-11-01 12:28:25 +0100

    generated components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkComponent } from './work/work.component';
+import { ConnectComponent } from './connect/connect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    WorkComponent,
+    ConnectComponent
   ],
   imports: [
     BrowserModule
