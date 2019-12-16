7d8a514185766fd1e07ce66b60d265dbd5b86cad
IhuomaO <logzimalone@gmail.com>
2019-12-16 11:37:58 +0100

    edited portfolio

--- src/app/work/work.component.ts
+++ src/app/work/work.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-work',
   templateUrl: './work.component.html',
   styleUrls: ['./work.component.css']
 })
-export class WorkComponent {
+export class WorkComponent implements OnInit {
 
-  constructor() {
+  constructor() { }
 
+  ngOnInit() {
   }
 
 }
