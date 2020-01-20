b074b92fd089a72680233281f92d8d93d8d9576a
IhuomaO <logzimalone@gmail.com>
2020-01-20 08:18:35 +0100

    changed base href

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule,  } from '@angular/router';
+import { RouterModule, } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
